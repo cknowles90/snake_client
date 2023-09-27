817939a4a0efe31fe0b940016e2585e2e5e58bdb
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-09-27 13:37:06 -0700

    added two functions to play.js (setupInput, handleUserInput) prepping for keyboard commands

--- play.js
+++ play.js
@@ -1,5 +1,25 @@
 const connect = require("./client");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  
+  const handleUserInput = (data) => { 
+    if (data === '\u0003') {
+      console.log("Terminating the game...")
+      process.exit();
+  };
+};
+  
+  stdin.on("data", handleUserInput);
+  
+  return stdin;
+};
+
+setupInput();
+
 
 console.log("Connecting ...");
 connect();
