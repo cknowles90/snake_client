cdad29a6cb472080d837b49b2a869e24866d486d
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-09-27 12:29:10 -0700

    Initial connecting to the server - included some feedback to the user regarding connection status

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    port: 50541,
+    host: "165.227.47.243"
+  });
+
+  conn.on("connect", () => {
+    console.log("I'm a sneeeek")
+  });
+  
+  conn.on("data", () => {
+  console.log("you ded cuz you idled");
+  });
+
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
