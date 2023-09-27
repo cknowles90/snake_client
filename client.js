f27292d984bdeee1c74084f8f0bda087de2d20f0
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-09-27 13:10:00 -0700

    played around with movement functions, and setTimeout/setIntervals for said movements

--- client.js
+++ client.js
@@ -11,14 +11,38 @@ const connect = function() {
   conn.on("connect", () => {
     console.log("Successfully slithered into the game...");
   });
+    
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    //   console.log("moving up)");
+    // }, 50);
+
+    //   () => {
+    //   setTimeout(() => {
+    //   }, 50);
+    //   console.log("Move up one square (unless facing down");
+    // });
 
   conn.write("Name: ", () => {
     console.log("Mum");
   });
   
+  conn.write("Move: down", () => {
+    console.log("Move down one square (unless facing up)");
+  });
+
+  conn.write("Move: left", () => {
+    console.log("Move left one square (unless facing right)");
+  });
+
+  conn.write("Move: right", () => {
+    console.log("Move right one square (unless facing left)");
+  });
+
   conn.on("data", () => {
   console.log("you ded cuz you idled");
   });
+
 
   conn.setEncoding("utf8");
 
